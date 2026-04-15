efd8a88d498a4f2bbeb317a32c670c4116f4330d
travelstopagency-debug <travelstop.agency@gmail.com>
2026-04-15 12:15:51 -0600

    Update next.config.ts to add turbopack root config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default withNextIntl(nextConfig);
